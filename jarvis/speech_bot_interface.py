36442769bd248713c906584cf1fbc646b84a0c30
Winston Carlile <winston.carlile@gmail.com>
2017-03-08 23:55:23 -0600

    initial commit

---
+++ jarvis/speech_bot_interface.py
@@ -0,0 +1,39 @@
+from gtts import gTTS
+from transcribe import transcribe_file
+import os
+import sys
+import subprocess
+import neural_net as nn
+import performAction
+
+
+def getTextFromSpeech(filePath):
+    text = subprocess.check_output([sys.executable, "transcribe.py", filePath])
+    return text[text.index(':') + 1 :]
+
+
+def runBot(request):
+    return request
+
+
+def speakResponse(response):
+    tts = gTTS(text=response, lang='en')
+    tts.save("resources/bot_output.wav")
+    os.system("aplay resources/bot_output.wav")
+
+
+def init():
+    nn.train()
+
+
+def getResponse(filename):
+    request = getTextFromSpeech(filename)
+    responseText = performAction.performBotAction(nn.response(request))
+    response = runBot(request)
+    speakResponse(response)
+
+    
+if __name__ == '__main__':
+    nn.train()
+    filename = sys.argv[1]
+    
